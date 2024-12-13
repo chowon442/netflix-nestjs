973cbd051b1a8c3049b31eea3bb11397404fa975
chowon442 <chowon442@gmail.com>
2024-12-14 02:25:41 +0900

    feat: create basic movie api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,55 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('movie')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getMovies() {
+    return [
+      {
+        id: 1,
+        name: '해리포터',
+        character: ['해리포터'],
+      },
+      {
+        id: 2,
+        name: '반지의 제왕',
+        character: ['프로도', '사우론', '간달프'],
+      },
+    ];
+  }
+
+  @Get(':id')
+  getMovie() {
+    return {
+      id: 1,
+      name: '해리포터',
+      character: ['해리포터', '헤르미온느', '론'],
+    }
+  }
+
+  @Post()
+  postMovie() {
+    return {
+      id: 3,
+      name: '스타워즈',
+      character: ['루크', '레이어', '레이'],
+    }
+  }
+
+  @Patch(':id')
+  patchMovie() {
+    return {
+      id: 1,
+      name: '해리포터',
+      character: ['해리포터', '헤르미온느', '론', '말포이'],
+    }
+  }
+
+  @Delete(':id')
+  deleteMovie() {
+    return 3;
   }
 }
